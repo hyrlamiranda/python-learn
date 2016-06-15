a31a58e253cca639cdba157e36f56f905d342478
hyrlamiranda <hyrlammiranda@gmail.com>
2016-06-15 23:14:53 +0100

    modified

--- choice.py
+++ choice.py
@@ -1,10 +1,11 @@
 #Write a program that asks the user for a number n and gives him the possibility to 
 #choose between computing the sum and computing the product of 1,…,n.
 
-sum = input("Enter a number")
-sum1 = raw_input
+print ("Enter a number")
+number = 1
 
-print "1 to sum and 2 to product"
+print "1 to sum, 2 to product"
 choice =
 
-print(1,sum)
+
+print (1..number,2 choice)
