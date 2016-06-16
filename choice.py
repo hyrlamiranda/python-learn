f49886339589e910b6539fcf202b440be539cdba
hyrlamiranda <hyrlammiranda@gmail.com>
2016-06-16 22:53:01 +0100

    modified

--- choice.py
+++ choice.py
@@ -1,3 +1,6 @@
+#Write a program that asks the user for a number n and gives him the possibility to choose between #computing the sum and computing the product of 1,…,n.
+
+
 s = int(input("Please enter a number:\n"))
     p = input("Choose sum or product. Please input s for sum, p for product, or e for exit:\n")
     if p in ("sum", "s"):
