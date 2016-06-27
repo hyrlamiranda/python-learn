763bc59d962aa66e5315b3d2f3d97c97c6185ea7
hyrlamiranda <hyrlammiranda@gmail.com>
2016-06-27 18:33:26 +0200

    More exercises in Python

---
+++ morepython/map.py
@@ -0,0 +1,39 @@
+## Python tutorial by Derek Banas 
+#Code
+#Learn-program(Python)
+#####
+
+import random
+import sys
+import os
+
+# DICTIONARY or MAP -------------
+# Made up of values with a unique key for each value
+# Similar to lists, but you can't join dicts with a +
+
+super_villains = {'Fiddler': 'Isaac Bowin',
+                  'Captain Cold': 'Leonard Snart',
+                  'Weather Wizard': 'Mark Mardon',
+                  'Mirror Master': 'Sam Scudder',
+                  'Pied Piper': 'Thomas Peterson'}
+
+print(super_villains['Captain Cold'])
+
+# Delete an entry
+del super_villains['Fiddler']
+print(super_villains)
+
+# Replace a value
+super_villains['Pied Piper'] = 'Hartley Rathaway'
+
+# Print the number of items in the dictionary
+print(len(super_villains))
+
+# Get the value for the passed key
+print(super_villains.get("Pied Piper"))
+
+# Get a list of dictionary keys
+print(super_villains.keys())
+
+# Get a list of dictionary values
+print(super_villains.values())
