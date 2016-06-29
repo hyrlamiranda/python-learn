90b35d2e6f7e1be407a160ebe99f428716756151
hyrlamiranda <hyrlammiranda@gmail.com>
2016-06-29 18:14:30 +0200

    More exercises in Python

---
+++ morepython/tuples.py
@@ -0,0 +1,21 @@
+## Python tutorial by Derek Banas 
+#Code
+#Learn-program(Python)
+#####
+
+
+#TUPLES
+# Values in a tuple can't change like lists
+# A tuple is a sequence of immutable Python objects. Tuples are sequences, just like lists. The differences between tuples and lists are, the tuples cannot be changed unlike lists and tuples use parentheses, whereas lists use square brackets.
+#Creating a tuple is as simple as putting different comma-separated values. Optionally you can put these comma-separated values between parentheses also. For example −
+
+pi_tuple = (3, 1, 4, 1, 5, 9)
+
+# Convert tuple into a list
+new_tuple = list(pi_tuple)
+
+# Convert a list into a tuple
+# new_list = tuple(grocery_list)
+
+# tuples also have len(tuple), min(tuple) and max(tuple)
+
