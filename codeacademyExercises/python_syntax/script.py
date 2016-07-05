49812e3537dbfbf66e4990d666e10864258ec75b
hyrlamiranda <hyrlammiranda@gmail.com>
2016-07-05 16:37:00 +0200

    Codecademy exercise answers in Python

---
+++ codeacademyExercises/python_syntax/script.py
@@ -0,0 +1,43 @@
+print "Welcome to Python!"
+
+
+#Variables
+
+# Write your code below!
+
+my_variable = 10
+
+
+#Booleans
+# Set the variables to the values listed in the instructions!
+
+my_int = 7
+my_float = 1.23
+my_bool = True
+
+# my_int is set to 7 below. What do you think
+# will happen if we reset it to 3 and print the result?
+
+my_int = 7
+
+# Change the value of my_int to 3 on line 8!
+
+my_int = 3
+
+# Here's some code that will print my_int to the console:
+# The print keyword will be covered in detail soon!
+
+print my_int
+
+#whitespace
+
+def spam():
+    eggs = 12
+    return eggs
+        
+print spam()
+
+#A Matter of Interpretation
+
+spam = True
+eggs = False
