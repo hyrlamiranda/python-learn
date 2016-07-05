983826a98cec9b2de1df626c2c8e4fccc8fbefe2
hyrlamiranda <hyrlammiranda@gmail.com>
2016-07-05 16:46:44 +0200

    Codecademy exercise answers in Python

--- codeacademyExercises/python_syntax/script.py
+++ codeacademyExercises/python_syntax/script.py
@@ -1,12 +1,10 @@
 print "Welcome to Python!"
 
-
 #Variables
 
 # Write your code below!
 
 my_variable = 10
-
 
 #Booleans
 # Set the variables to the values listed in the instructions!
@@ -41,3 +39,33 @@ print spam()
 
 spam = True
 eggs = False
+
+#Single Line Comments
+# What is the value of mysterious_variable?
+
+mysterious_variable = 42
+
+#multi-line comments
+
+""" I like cherry Blossom"""
+
+#math
+# Set count_to equal to the sum of two big numbers
+
+count_to = 4 + 4
+
+print count_to
+
+#exponentiation
+#Set eggs equal to 100 using exponentiation on line 3!
+
+eggs = 10 ** 2
+
+print eggs
+
+#Modulo
+#Set spam equal to 1 using modulo on line 3!
+
+spam = 5%2
+
+print spam
