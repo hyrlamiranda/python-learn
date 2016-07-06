f1825274b142f1a332333ede0739c9a2361579a7
hyrlamiranda <hyrlammiranda@gmail.com>
2016-07-06 22:05:42 +0200

    Codecademy Strings

---
+++ codeacademyExercises/strings/script3.py
@@ -0,0 +1,67 @@
+#Create a new variable brian and assign it the string "Hello life!".
+brian = "Hello life!"
+
+# Assign your variables below, each on its own line!
+caesar = "Graham"
+praline = "John"
+viking = "Teresa"
+
+# Put your variables above this line
+print caesar
+print praline
+print viking
+
+# The string below is broken. Fix it using the escape backslash!
+print 'This isn\'t flying, this is falling with style!'
+
+"""
+The string "PYTHON" has six characters,
+numbered 0 to 5, as shown below:
+
++---+---+---+---+---+---+
+| P | Y | T | H | O | N |
++---+---+---+---+---+---+
+  0   1   2   3   4   5
+
+So if you wanted "Y", you could just type
+"PYTHON"[1] (always start counting from 0!)
+"""
+fifth_letter = "MONTY"[4]
+print fifth_letter
+
+#On line 1, create a variable named parrot and set it to the string "Norwegian Blue". On line 2, type len(parrot) after the word print, like so: print len(parrot).
+#The output will be the number of letters in "Norwegian Blue"!
+parrot = "Norwegian Blue"
+len(parrot)
+print 
+print len(parrot)
+
+
+#Call lower() on parrot (after print) on line 3 in the editor.
+parrot = "Norwegian Blue"
+
+print parrot.lower()
+
+#Call upper() on parrot (after print on line 3) in order to capitalize all the characters in the string!
+parrot = "norwegian blue"
+print parrot.upper()
+
+#Create a variable pi and set it to 3.14 on line 4. Call str(pi) on line 5, after print.
+"""Declare and assign your variable on line 4,
+then call your method on line 5!"""
+
+pi = 3.14
+print str(pi)
+
+#On line 3, call the len() function with the argument ministry. On line 4, invoke the ministry's .upper() function.
+ministry = "The Ministry of Silly Walks"
+
+print len(ministry)
+print ministry.upper()
+
+#Print "Monty Python" to the console.
+"""Tell Python to print "Monty Python"
+to the console on line 4!"""
+
+print "Monty Python"
+
