ff35c5638baada0c7e636429134995ad7d8d4db5
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-02 12:25:42 +0100

    More exercises Python Lists and Dictionaries codeacademy

--- codeacademyExercises/lists_dictionaries/script5.py
+++ codeacademyExercises/lists_dictionaries/script5.py
@@ -139,3 +139,32 @@ print zoo_animals
 
 backpack = ['xylophone', 'dagger', 'tent', 'bread loaf']
 backpack.remove("dagger")
+
+#1.Add a key to inventory called 'pocket'
+#2.Set the value of 'pocket' to be a list consisting of the strings 'seashell', 'strange berry', and 'lint'
+#3.sort() the items in the list stored under the 'backpack' key
+#4.Then .remove('dagger') from the list of items stored under the 'backpack' key
+#5.Add 50 to the number stored under the 'gold' key
+
+inventory = {
+    'gold' : 500,
+    'pouch' : ['flint', 'twine', 'gemstone'], # Assigned a new list to 'pouch' key
+    'backpack' : ['xylophone','dagger', 'bedroll','bread loaf']
+}
+
+# Adding a key 'burlap bag' and assigning a list to it
+inventory['burlap bag'] = ['apple', 'small ruby', 'three-toed sloth']
+
+# Sorting the list found under the key 'pouch'
+inventory['pouch'].sort() 
+
+#mycode 
+#Steps 1 and 2
+inventory["pocket"] = ["seashell","strange berry", "lint"]
+#Step 3
+inventory["backpack"].sort()
+#Steps 4 and 5
+inventory['backpack'].remove('dagger')
+inventory['gold'] += 50
+print inventory
+
