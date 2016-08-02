aea53d45d28859cdc40eafd8de95ef292e39f050
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-02 11:50:01 +0100

    More exercises Python Lists and Dictionaries codeacademy

--- codeacademyExercises/lists_dictionaries/script5.py
+++ codeacademyExercises/lists_dictionaries/script5.py
@@ -114,4 +114,23 @@ print "There are " + str(len(menu)) + " items on the menu."
 print menu
 
 
+#Delete the 'Sloth' and 'Bengal Tiger' items from zoo_animals using del.
+#Set the value associated with 'Rockhopper Penguin' to anything other than 'Arctic Exhibit'.
 
+# key - animal_name : value - location 
+zoo_animals = { 'Unicorn' : 'Cotton Candy House',
+'Sloth' : 'Rainforest Exhibit',
+'Bengal Tiger' : 'Jungle House',
+'Atlantic Puffin' : 'Arctic Exhibit',
+'Rockhopper Penguin' : 'Arctic Exhibit'}
+# A dictionary (or list) declaration may break across multiple lines
+
+# Removing the 'Unicorn' entry. (Unicorns are incredibly expensive.)
+del zoo_animals['Unicorn']
+
+#Mycode!
+del zoo_animals["Sloth"]
+del zoo_animals["Bengal Tiger"]
+zoo_animals["Rockhopper Penguin"] = 'Other place'
+
+print zoo_animals
