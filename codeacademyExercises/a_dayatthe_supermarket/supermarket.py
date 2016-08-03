9f66055f8c9dd161319b7d2fc1549088a8921fb2
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-03 13:26:12 +0100

    A Day at the Supermarket

--- codeacademyExercises/a_dayatthe_supermarket/supermarket.py
+++ codeacademyExercises/a_dayatthe_supermarket/supermarket.py
@@ -1,11 +1,11 @@
 #A Day at the Supermarket
 
 names = ["Adam","Alex","Mariah","Martine","Columbus"]
-
+#loop to print out all of the elements in the list names.
 for names in names: 
     print names
 
-
+#loop to go through the webster dictionary and print out all of the definitions.
 webster = {
 	"Aardvark" : "A star of a popular children's cartoon show.",
     "Baa" : "The sound a goat makes.",
@@ -13,10 +13,10 @@ webster = {
     "Dab": "A small amount."
 }
 
-# mycode
 for key in webster:
     print webster[key]
 
+#Control Flow and Looping
 a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
 
 for entry in a:
