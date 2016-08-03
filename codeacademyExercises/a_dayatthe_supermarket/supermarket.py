114c3a1f384e9c353c4d6f98497848d8f94cb163
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-03 13:24:13 +0100

    A Day at the Supermarket

---
+++ codeacademyExercises/a_dayatthe_supermarket/supermarket.py
@@ -0,0 +1,26 @@
+#A Day at the Supermarket
+
+names = ["Adam","Alex","Mariah","Martine","Columbus"]
+
+for names in names: 
+    print names
+
+
+webster = {
+	"Aardvark" : "A star of a popular children's cartoon show.",
+    "Baa" : "The sound a goat makes.",
+    "Carpet": "Goes on the floor.",
+    "Dab": "A small amount."
+}
+
+# mycode
+for key in webster:
+    print webster[key]
+
+a = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+for entry in a:
+   if entry % 2 == 0:
+        print entry
+   else:
+        pass
