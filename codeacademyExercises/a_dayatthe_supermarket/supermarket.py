326a90be62108050da8bf2825c9c6c7cd1f8248c
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-04 12:46:58 +0100

    Shopping at the Market

--- codeacademyExercises/a_dayatthe_supermarket/supermarket.py
+++ codeacademyExercises/a_dayatthe_supermarket/supermarket.py
@@ -40,19 +40,9 @@ print fizz_count(["fizz","cat","fizz"])
 
 # new dictionary called prices using {}
 #mycode
-prices = {
-    "banana": 4,
-    "apple": 2,
-    "orange": 1.5,
-    "pear": 3
-}
+prices = { "banana": 4,"apple": 2,"orange": 1.5,"pear": 3}
 #stock dictionary
-stock = {
-    "banana": 6,
-    "apple": 0,
-    "orange": 32,
-    "pear": 15
-}
+stock = {"banana": 6, "apple": 0,"orange": 32,"pear": 15}
 
 #Keeping Track of the Produce
 for fruits in prices:
@@ -79,3 +69,19 @@ def compute_bill(food):
     return total
 
 print compute_bill(groceries)
+
+#Stocking Out
+
+#mycode
+#groceries = ["banana", "orange","apple"]
+#stock = {"banana": 6,"apple": 0,"orange": 32,"pear": 15}
+#prices = {"banana": 4,"apple": 2,"orange": 1.5,"pear": 3}
+
+def compute_bill(food):
+    total = 0
+    for item in food:  
+        if stock[item] > 0: 
+            total += prices[item] 
+            stock[item] -= 1
+    return total
+
