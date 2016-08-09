f98ab771cae9306d371a0ae20898cb871a75c03f
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-09 12:11:11 +0100

    new exercises about Lists and Functions

---
+++ codeacademyExercises/lists_functions/lists_functions.py
@@ -0,0 +1,20 @@
+#Add the code to print out the second element in the list
+
+n = [1, 3, 5]
+
+#myCode
+print  n[1]
+
+
+#On line 3, multiply the second element of the n list by 5
+#Overwrite the second element with that result.
+#Make sure to print the list when you are done!
+# Code
+n[1] = 3 * 5
+print n
+
+#Append the number 4 to the end of the list n.
+n = [1, 3, 5]
+# Append the number 4 here
+n.append(4)
+print n
