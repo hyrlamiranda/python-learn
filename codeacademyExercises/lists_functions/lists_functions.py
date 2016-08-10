acec6bf70282e20b6cff03c0a1af24d8b039baa6
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-10 11:10:27 +0100

    Exercises about Lists and Functions

--- codeacademyExercises/lists_functions/lists_functions.py
+++ codeacademyExercises/lists_functions/lists_functions.py
@@ -37,3 +37,13 @@ def my_function(x):
     return x * 3
 
 print my_function(number)
+
+#Define a function called add_function that has 2 parameters x and y and adds them together.
+
+m = 5
+n = 13
+# Add add_function here!
+def add_function(x, y):
+    return x + y
+
+print add_function(m, n)
