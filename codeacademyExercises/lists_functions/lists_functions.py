651e5415fb23ff2f9dcc623973eca632bf7b1085
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-11 17:10:22 +0100

    Exercises about Lists and Functions, more

--- codeacademyExercises/lists_functions/lists_functions.py
+++ codeacademyExercises/lists_functions/lists_functions.py
@@ -129,3 +129,14 @@ def my_function(x):
     return x
 
 print my_function(range(3))
+
+#Create a function called total that adds up all the elements of an arbitrary list and returns that count, using the existing code as a hint. 
+#Use a for loop so it can be used for any size list.
+
+n = [3, 5, 7]
+
+def total(numbers):
+    result = 0
+    for i in range(len(numbers)):
+        result = result + numbers[i] #result += numbers[i] another way
+    return result
