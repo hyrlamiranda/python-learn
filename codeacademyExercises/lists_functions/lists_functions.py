10980908a5a6a2318a973674f48d128eea91059e
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-11 16:56:27 +0100

    Exercises about Lists and Functions, more

--- codeacademyExercises/lists_functions/lists_functions.py
+++ codeacademyExercises/lists_functions/lists_functions.py
@@ -107,3 +107,25 @@ def print_list(x):
     for i in range(0, len(x)):
         print x[i]
 print print_list(n)
+
+#Create a function called double_list that takes a single argument x
+#(which will be a list) and multiplies each element by 2 and returns that list. Use the existing code as a scaffold.
+n = [3, 5, 7]
+
+def double_list(x):
+    for i in range(0, len(x)):
+        x[i] = x[i] * 2
+# Don't forget to return your new list!
+    return x
+print double_list(n)
+
+#with a range() that returns a list containing [0, 1, 2].
+
+x = [0, 1, 2]
+
+def my_function(x):
+    for i in range(0, len(x)):
+        x[i] = x[i] * 2
+    return x
+
+print my_function(range(3))
