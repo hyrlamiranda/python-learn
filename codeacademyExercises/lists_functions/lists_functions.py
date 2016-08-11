968af419f15d33cd1f2c6d2cf53a253db8e0abc6
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-11 16:43:34 +0100

    Exercises about Lists and Functions, more

--- codeacademyExercises/lists_functions/lists_functions.py
+++ codeacademyExercises/lists_functions/lists_functions.py
@@ -97,3 +97,13 @@ def list_extender(lst):
     return lst
 
 print list_extender(n)
+
+
+#Define a function called print_list that has one argument called x.
+#Inside that function, print out each element one by one. Use the existing code as a scaffold.
+#Then call your function with the argument n.
+n = [3, 5, 7]
+def print_list(x):
+    for i in range(0, len(x)):
+        print x[i]
+print print_list(n)
