e41d1012eceaecbcbf0cf59b3aa8ccf87b06e15f
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-14 18:12:52 +0100

    Custom Print

--- codeacademyExercises/battleship/battleship.py
+++ codeacademyExercises/battleship/battleship.py
@@ -6,3 +6,16 @@ board = []
 for i in range(5):
     board.append(["O"] * 5) 
 print board
+
+#First, delete your existing print statement.
+#Then, define a function named print_board with a single argument, board.
+#Inside the function, write a for loop to iterates through each row in board and print it to the screen.
+#Call your function with board to make sure it works.
+board = []
+# range(5) is a shortcut for range(0, 5)
+for i in range(5):
+    board.append(["O"] * 5) 
+def print_board(board):
+    for row in board:
+        print row
+print print_board(board)
