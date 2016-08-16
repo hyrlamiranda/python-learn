8e29567eeb8add4a59e61ad1c8fa21b63decae70
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-16 11:04:49 +0100

    Battleship in a random location on the board

--- codeacademyExercises/battleship/battleship.py
+++ codeacademyExercises/battleship/battleship.py
@@ -31,3 +31,27 @@ def print_board(board):
         #mycode
         print " ".join(row)
 print print_board(board)
+
+#Define two new functions, random_row and random_col, that each take board as input.
+#These functions should return a random row index and a random column index from your board, respectively. Use randint(0, len(board) - 1).
+#Call each function on board.
+
+from random import randint 
+
+board = []
+
+for x in range(0, 5):
+    board.append(["O"] * 5)
+
+def print_board(board):
+    for row in board:
+        print " ".join(row)
+
+# Add your code below!My code
+def random_row(board):
+    return randint(0, len(board)-1)
+def random_col(board):
+    return randint(0, len(board[0])-1)
+    
+print random_row(board)
+print random_col(board)
