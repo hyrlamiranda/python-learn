d853fa3913465ddfd4847ce3f779fee5b5b80f03
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-19 15:03:03 +0100

    While loop is to check user input to see if it is valid.

--- codeacademyExercises/loops/loopswhile .py
+++ codeacademyExercises/loops/loopswhile .py
@@ -25,3 +25,10 @@ while num <= 10:  # Fill in the condition
     print num ** 2
     # Increment num (make sure to do this!)
     num += 1
+
+#Fill in the loop condition so the user will be prompted for a choice over and over while choice does not equal 'y' and choice does not equal 'n'
+
+choice = raw_input('Enjoying the course? (y/n)')
+
+while choice != "y" and choice != "n":  # Fill in the condition (before the colon)
+    choice = raw_input("Sorry, I didn't catch that. Enter again: ")
