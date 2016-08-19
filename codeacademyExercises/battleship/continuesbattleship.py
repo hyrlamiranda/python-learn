f9b870e8c60a93df810d6f5b268183535b7fcb61
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-19 13:30:11 +0100

    We can play Battleship

--- codeacademyExercises/battleship/continuesbattleship.py
+++ codeacademyExercises/battleship/continuesbattleship.py
@@ -2,7 +2,6 @@
 #Add a for loop that repeats the guessing and checking part of your game for 4 turns, like the example above.
 #At the beginning of each iteration, print "Turn", turn + 1 to let the player know what turn they are on.
 #Indent everything that should be repeated.
-
 from random import randint
 
 board = []
@@ -29,29 +28,25 @@ print ship_row
 print ship_col
 
 for turn in range(4):
-    print "Turn", turn + 1
-
-guess_row = int(raw_input("Guess Row:"))
-guess_col = int(raw_input("Guess Col:"))
-
-if guess_row == ship_row and guess_col == ship_col:
-    print "Congratulations! You sunk my battleship!"
-else:
-    if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
-        print "Oops, that's not even in the ocean."
-    elif(board[guess_row][guess_col] == "X"):
-        print "You guessed that one already."
-    else:
-        print "You missed my battleship!"
-        board[guess_row][guess_col] = "X"
-    # Print (turn + 1) here!
     print_board(board)
-
+    print "Turn", turn + 1
+    guess_row = int(raw_input("Guess Row:"))    
+    guess_col = int(raw_input("Guess Col:"))
+    if guess_row == ship_row and guess_col == ship_col:
+        print "Congratulations! You sunk my battleship!"
+    else:
+        if (guess_row < 0 or guess_row > 4) or (guess_col < 0 or guess_col > 4):
+            print "Oops, that's not even in the ocean."
+        elif(board[guess_row][guess_col] == "X"):
+            print "You guessed that one already."
 #Game Over
 #Add an if statement that checks to see if the user is out of guesses.
 #Put it under the else that accounts for misses.
 #Put it after the if/elif/else statements that check for the reason the player missed. We want "Game Over" to print regardless of the reason.
 #If turn equals 3, print "Game Over".
-if turn == 3:
-    print "Game Over"
+        elif turn == 3:
+            print "Game Over"
+            #break to get out of a for loop.
+            break
+
 
