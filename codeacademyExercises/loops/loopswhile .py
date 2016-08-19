bfccc9fe7e0d825870d92b5c8f61b10b75a89e67
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-19 14:55:24 +0100

    Including arithmetic operations.

--- codeacademyExercises/loops/loopswhile .py
+++ codeacademyExercises/loops/loopswhile .py
@@ -7,3 +7,21 @@ if count < 5:
 while count <= 9:
     print "Hello, I am a while and count is", count
     count += 1
+# loop checks its condition, and when it stops executing
+loop_condition = True
+
+while loop_condition:
+    print "I am a loop"
+    loop_condition = False
+
+#Create a while loop that prints out all the numbers from 1 to 10 squared (1, 4, 9, 16, ... , 100), each on their own line.
+#Fill in the blank space so that our while loop goes from 1 to 10 inclusive.
+#Inside the loop, print the value of num squared. The syntax for squaring a number is num ** 2.
+#Increment num
+num = 1
+
+while num <= 10:  # Fill in the condition
+    # Print num squared
+    print num ** 2
+    # Increment num (make sure to do this!)
+    num += 1
