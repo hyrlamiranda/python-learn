e07412cf9232b9a3546fb7790d3296db502dcb4d
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-22 13:42:44 +0100

    Character after r print

--- codeacademyExercises/loops/loopswhile.py
+++ codeacademyExercises/loops/loopswhile.py
@@ -106,3 +106,17 @@ word = "eggs!"
 # my code here!
 for d in word:
     print d
+
+#Do the following for each character in the phrase.
+#If char is an 'A' or char is an 'a', print 'X', instead of char. Make sure to include the trailing comma.
+#Otherwise (else:), please print char, with the trailing comma.
+
+phrase = "A bird in the hand..."
+
+# Add your for loop
+for char in phrase:
+    if char == "A" or char == "a":
+        print "X",
+    else: print char,
+#Don't delete this print statement!
+print 
