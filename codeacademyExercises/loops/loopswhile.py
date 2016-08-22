aa75cfc06e6482dd6bf0113c67b7c2e3c60ec736
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-22 12:22:00 +0100

    Using while/else similar the game Battleship

--- codeacademyExercises/loops/loopswhile.py
+++ codeacademyExercises/loops/loopswhile.py
@@ -57,3 +57,24 @@ while count < 3:
     count += 1
 else:
     print "You win!"    
+
+#Use a while loop to let the user keep guessing so long as guesses_left is greater than zero.
+#Ask the user for their guess, just like the second example above.
+#If they guess correctly, print 'You win!' and break.
+#Decrement guesses_left by one.
+#Use an else: case after your while loop to print You lose..
+from random import randint
+
+# Generates a number from 1 through 10 inclusive
+random_number = randint(1, 10)
+
+guesses_left = 3
+# Start your game!
+while guesses_left > 0:
+    guess = int(raw_input("Your guess: "))
+    if guess == random_number:
+        print "You win!"
+        break
+    guesses_left = guesses_left - 1
+else:
+    print "You lose!"
