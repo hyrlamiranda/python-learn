fd1565c47e353bb62dfebaf75bc87fbffba49809
hyrlamiranda <hyrlammiranda@gmail.com>
2016-08-31 12:28:54 +0100

    Multiple lists

--- codeacademyExercises/loops/loopswhile.py
+++ codeacademyExercises/loops/loopswhile.py
@@ -140,4 +140,14 @@ d = {'a': 'apple', 'b': 'berry', 'c': 'cherry'}
 
 for key in d:
     # Code
-    print key, (d[key])    
+    print key, (d[key])  
+
+
+#  Compare each pair of elements and print the larger of the two.
+
+list_a = [3, 9, 17, 15, 19]
+list_b = [2, 4, 8, 10, 30, 40, 50, 60, 70, 80, 90]
+
+for a, b in zip(list_a, list_b):
+    # Add your code here!
+    print max(a, b)
