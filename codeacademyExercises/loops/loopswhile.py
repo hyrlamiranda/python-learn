7efcb00427ae58bbc9ded781fa8f0635054c2e8a
hyrlamiranda <hyrlammiranda@gmail.com>
2016-09-01 11:37:50 +0100

    For/else

--- codeacademyExercises/loops/loopswhile.py
+++ codeacademyExercises/loops/loopswhile.py
@@ -159,3 +159,17 @@ for a, b in zip(list_a, list_b):
         print a
     else:
         print b
+
+
+#For / else
+
+fruits = ['banana', 'apple', 'orange', 'tomato', 'pear', 'grape']
+
+print 'You have...'
+for f in fruits:
+    if f == 'tomato':
+        print 'A tomato is not a fruit!' # (It actually is.)
+        break
+    print 'A', f
+else:
+    print 'A fine selection of fruits!'        
