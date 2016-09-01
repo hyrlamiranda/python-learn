55fd54330129526d17c0de9e71f5d4ca2abc2de3
hyrlamiranda <hyrlammiranda@gmail.com>
2016-09-01 11:36:37 +0100

    Multiple lists

--- codeacademyExercises/loops/loopswhile.py
+++ codeacademyExercises/loops/loopswhile.py
@@ -151,3 +151,11 @@ list_b = [2, 4, 8, 10, 30, 40, 50, 60, 70, 80, 90]
 for a, b in zip(list_a, list_b):
     # Add your code here!
     print max(a, b)
+
+#Or
+for a, b in zip(list_a, list_b):
+    # Add your code here!
+    if a > b:
+        print a
+    else:
+        print b
